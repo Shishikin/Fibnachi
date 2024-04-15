7e1c4644b179c4aa51dd349d498ea2a5d47f77fd
Shishikin <mvg1313@mail.ru>
2024-04-16 00:00:38 +0300

    Добавьте файлы проекта.

---
+++ Fibnachi.cpp
@@ -0,0 +1,72 @@
+﻿// Fibnachi.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+bool FixFlow()
+{
+    if (std::cin.fail()) {
+        std::cin.clear();
+        std::cin.ignore();
+        fflush(stdin);
+        return true;
+        //        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+template <typename T>
+void Input(T& side1, const std::string& message)
+{
+    do
+    {
+        std::cout << message << '\n';
+        std::cin >> side1;
+    } while (FixFlow());
+}
+
+unsigned int Fibonachi(unsigned int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
+
+int main()
+{
+#ifdef _WIN32
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+#endif
+    unsigned int n = 0;
+    Input(n, "Введите порядковый номер последоваельности Фибоначи ");
+    std::cout << "Число Фибоначи ";
+//    std::cout << n;
+    std::cout << Fibonachi(n);
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
